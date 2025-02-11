9e35b9711660c33bd69989fdbdb6ef64bf83c016
Sergio Loza <lozass@gmail.com>
2025-02-11 09:02:37 -0500

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationdotnetmvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
